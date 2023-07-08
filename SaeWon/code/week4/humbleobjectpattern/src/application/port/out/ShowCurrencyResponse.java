a47f1c9822b28303e92f37e4a159f4b50787998d
gggorock <iojkgh88@gmail.com>
2023-07-08 12:31:46 +0900

    code fix : 4주차 험블객체 테스트코드 추가

--- SaeWon/code/week4/humbleobjectpattern/src/application/port/out/ShowCurrencyResponse.java
+++ SaeWon/code/week4/humbleobjectpattern/src/application/port/out/ShowCurrencyResponse.java
@@ -1,3 +1,10 @@
 package application.port.out;
 
-public record ShowCurrencyResponse (String amount){}
+public record ShowCurrencyResponse (String amount){
+    @Override
+    public String toString() {
+        return "ShowCurrencyResponse{" +
+                "amount='" + amount + '\'' +
+                '}';
+    }
+}
